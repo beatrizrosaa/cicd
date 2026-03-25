0f4e7b6fbbb08a6ac3beb4e890e3994a359ac4e7
Davi Mattos Blumetti <davimblu@gmail.com>
2026-03-25 22:33:08 +0000

    Update file appTest.py

--- tests/appTest.py
+++ tests/appTest.py
@@ -10,3 +10,4 @@ class AppTest(unittest.TestCase):
         self.assertEqual(200, response.status_code, "Erro no test_http_code!")
         self.assertEqual("<h1>Hello, I'm Alive!</h1>", response.get_data(as_text=True)
                           , "Erro no test_print_health_check!")
+                          
